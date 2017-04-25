63f3df400101e65369066e4fddbcb9027bb0aa1c
João Justo <jpjustonunes@gmail.com>
2017-04-25 15:46:19 +0100

    Add biography section

---
+++ components/Biography/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+import './index.scss';
+
+const Biography = () => (
+  <div className="Biography">
+    <div className="Biography-smallSection">
+      <h2 className="Biography-title">Biografia</h2>
+    </div>
+    <div className="Biography-largeSection">
+      <div className="Biography-navigation">
+        <a className="Biography-downloadButton">Download PDF</a>
+        <a className="Biography-fullButton">Ler Biografia Completa</a>
+      </div>
+      <div className="Biography-content">
+        José Eduardo é maestro titular da Orquestra Clássica do Centro, Orquestra Clássica da FEUP e do Coro do Círculo Portuense de Ópera.
+        Recentemente foi laureado com o 2º Prémio no Concurso Prémio Jovens Músicos, na categoria de Direção de Orquestra, tendo obtido igualmente o Prémio da Orquestra.
+        Iniciou os seus estudos musicais com o clarinete na sua cidade natal, V. N. Famalicão.. Mais tarde, continuou estudos na ARTAVE e ESMAE - Porto, onde se formou na classe do Prof. Antonio Saiote. Prosseguiu estudos em Direcção de Orquestra na Haute École de Musique de Genève (Suíça), com Laurent Gay e direcção coral com Celso Antunes.
+        Realizou masterclasses de direcção de orquestra com Jorma Panula, Antonio Saiote, Cesário Costa (...) formou na classe do Prof. Antonio Saiote. Prosseguiu estudos em Direcção de Orquestra na Haute École de Musique de Genève (Suíça), com Laurent Gay e direcção coral com Celso Antunes.
+        Realizou masterclasses de direcção de orquestra com Jorma Panula, Antonio Saiote, Cesário Costa (...).formou na classe do Prof. Antonio Saiote. Prosseguiu estudos em Direcção de Orquestra na Haute École de Musique de Genève (Suíça), com Laurent Gay e direcção coral com Celso Antunes.
+        Realizou masterclasses de direcção de orquestra com Jorma Panula, Antonio Saiote, Cesário Costa (...)..
+      </div>
+    </div>
+  </div>
+);
+
+export default Biography;
--- pages/index.js
+++ pages/index.js
@@ -3,12 +3,14 @@ import React from 'react';
 import Hero from '../components/Hero';
 import Agenda from '../components/Agenda';
 import News from '../components/News';
+import Biography from '../components/Biography';
 
 const Index = () => (
   <div className="Index">
     <Hero />
     <Agenda />
     <News />
+    <Biography />
   </div>
 );
 

aef7116115bf97512cb8ebc56242549ec3a4e403
João Justo <jpjustonunes@gmail.com>
2017-06-11 23:04:37 +0200

    Add footer

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -4,24 +4,28 @@ import './index.scss';
 import TwitterLogo from './twitter.svg';
 import FacebookLogo from './facebook.svg';
 
-const Footer = () => (
+const Footer = () =>
   <div className="Footer">
     <div className="Footer-column">
       <h2 className="Footer-title">Contacto</h2>
     </div>
     <div className="Footer-largeColumn">
-      <p>José Eduardo Gomes</p>
-      <p>gomes.jose83@gmail.com</p>
-      <div className="Footer-socialLinks">
-        <img src={FacebookLogo} className="Footer-socialLink" />
-        <img src={TwitterLogo} className="Footer-socialLink" />
+      <div className="Footer-content">
+        <div>
+          <p>José Eduardo Gomes</p>
+          <p>gomes.jose83@gmail.com</p>
+        </div>
+        <div className="Footer-socialLinks">
+          <img src={FacebookLogo} className="Footer-socialLink" />
+          <img src={TwitterLogo} className="Footer-socialLink" />
+        </div>
       </div>
       <p className="Footer-copywrite">
-        Copyright © 2017 José Eduardo Gomes. All Rights Reserved. Website by João Justo and João Andrade
+        Copyright © 2017 José Eduardo Gomes. All Rights Reserved. Website by
+        João Justo and João Andrade
       </p>
     </div>
     <div className="Footer-column" />
-  </div>
-);
+  </div>;
 
 export default Footer;

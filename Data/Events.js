50d45fb6f5193036aa4cdebbd46e957ab9d32f69
josegomes <gomes.jose83@gmail.com>
2017-07-17 22:12:52 +0100

    Update Events.js

--- Data/Events.js
+++ Data/Events.js
@@ -1,52 +1,13 @@
 export default [
   {
-    day: 25,
-    month: 5,
-    year: 2017,
-    date: '25 Maio',
-    location: 'Ponte 25 de abril, Lisboa',
-    name: 'Concerto Comemoração da Liberdade',
-    description:
-      'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua sed do eiusmod tempor incididunt ut labore et dolore magna Ut enim ad me ed do eiusmod tempor incididunt ut labore et dolore magna Ut enim ad me. ed do eiusmod tempor incididunt ut labore et dolore magna Ut enim ad me. ed do eiusmod tempor incididunt ut labore et dolore magna Ut enim ad me.'
-  },
-  {
-    day: 26,
-    month: 5,
-    year: 2017,
-    date: '26 Maio',
-    location: 'Ponte 25 de abril, Lisboa',
-    name: 'Concerto Comemoração da Liberdade',
-    description:
-      'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua sed do eiusmod tempor incididunt ut labore et dolore magna Ut enim ad me ed do eiusmod tempor incididunt ut labore et dolore magna Ut enim ad me. ed do eiusmod tempor incididunt ut labore et dolore magna Ut enim ad me. ed do eiusmod tempor incididunt ut labore et dolore magna Ut enim ad me.'
-  },
-  {
-    day: 27,
-    month: 5,
-    year: 2017,
-    date: '27 Maio',
-    location: 'Ponte 25 de abril, Lisboa',
-    name: 'Concerto Comemoração da Liberdade',
-    description:
-      'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua sed do eiusmod tempor incididunt ut labore et dolore magna Ut enim ad me ed do eiusmod tempor incididunt ut labore et dolore magna Ut enim ad me. ed do eiusmod tempor incididunt ut labore et dolore magna Ut enim ad me. ed do eiusmod tempor incididunt ut labore et dolore magna Ut enim ad me.'
-  },
-  {
-    day: 29,
-    month: 5,
-    year: 2017,
-    date: '29 Maio',
-    location: 'Ponte 25 de abril, Lisboa',
-    name: 'Concerto Comemoração da Liberdade',
-    description:
-      'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua sed do eiusmod tempor incididunt ut labore et dolore magna Ut enim ad me ed do eiusmod tempor incididunt ut labore et dolore magna Ut enim ad me. ed do eiusmod tempor incididunt ut labore et dolore magna Ut enim ad me. ed do eiusmod tempor incididunt ut labore et dolore magna Ut enim ad me.'
-  },
-  {
-    day: 1,
-    month: 5,
-    year: 2017,
-    date: '1 Maio',
-    location: 'Ponte 25 de abril, Lisboa',
-    name: 'Concerto Comemoração da Liberdade',
-    description:
-      'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua sed do eiusmod tempor incididunt ut labore et dolore magna Ut enim ad me ed do eiusmod tempor incididunt ut labore et dolore magna Ut enim ad me. ed do eiusmod tempor incididunt ut labore et dolore magna Ut enim ad me. ed do eiusmod tempor incididunt ut labore et dolore magna Ut enim ad me.'
+    day: 20,
+    month: 7,
+    year: 2017
+    date: '2o Julho 2017',
+    location: 'Fundação Calouste Gulbenkian, Lisboa',
+    name: 'Concerto pela Orquestra Gulbenkian, Dia do Fundador',
+    description:'A Orquestra Gulbenkian, dirigida por José Eduardo Gomes, com os solistas Agostinho Sequeira (percussão) e Haïg Sarikouyoumdjian (duduk), irá interpretar obras de Jennifer Higdon e de Ludwig van Beethoven'
+
+
+  
   }
-];

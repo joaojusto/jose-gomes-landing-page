d666379c6e657d65d6377d5ccc16c2695c351f8f
João Justo <joaojusto@users.noreply.github.com>
2017-07-17 23:14:52 +0200

    Fix data/events

--- Data/Events.js
+++ Data/Events.js
@@ -2,12 +2,9 @@ export default [
   {
     day: 20,
     month: 7,
-    year: 2017
-    date: '2o Julho 2017',
+    year: 2017,
+    date: '20 Julho 2017',
     location: 'Fundação Calouste Gulbenkian, Lisboa',
     name: 'Concerto pela Orquestra Gulbenkian, Dia do Fundador',
-    description:'A Orquestra Gulbenkian, dirigida por José Eduardo Gomes, com os solistas Agostinho Sequeira (percussão) e Haïg Sarikouyoumdjian (duduk), irá interpretar obras de Jennifer Higdon e de Ludwig van Beethoven'
-
-
-  
+    description:'A Orquestra Gulbenkian, dirigida por José Eduardo Gomes, com os solistas Agostinho Sequeira (percussão) e Haïg Sarikouyoumdjian (duduk), irá interpretar obras de Jennifer Higdon e de Ludwig van Beethoven'  
   }
